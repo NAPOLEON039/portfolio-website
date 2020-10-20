a4453f3bf483b61002eeb0cc09c7d6286a95dd81
Nihar Raote <nihar.raote@gmail.com>
2020-10-20 18:30:25 +0530

    Added navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <ul className="nav-items">
+                <li>About Me</li>
+                <li>Skills</li>
+                <li>Projects</li>
+                <li>Certifications</li>
+                <li>Resume</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
